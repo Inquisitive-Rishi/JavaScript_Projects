d9ce8661a017217c8884ffd5a837ed3cfa37542e
Rishi Raj <rishiraj007bgp@gmail.com>
2024-01-04 20:00:17 +0530

    finish project

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-const btnContainer = document.querySelector('#container')
-const playerScore = document.getElementById('#player-score')
-const aiScore = document.getElementById('#computer-score')
-const playerChoice = document.getElementById('#player-choice')
-const aiChoice = document.getElementById('#computer-choice')
+const btnContainer = document.querySelector('container')
+const playerScore = document.getElementById('player-score')
+const aiScore = document.getElementById('computer-score')
+const playerChoice = document.getElementById('player-choice')
+const aiChoice = document.getElementById('computer-choice')
 const buttons = document.querySelectorAll('#container button')
-const playerWinner = document.getElementById('#player-winner')
-const aiWinner = document.getElementById('#ai-winner')
+const roundResult = document.getElementById('round-result')
+const winner = document.getElementById('winner')
+const restartBtn = document.getElementById('restartBtn')
+
+const domElements = [playerScore, aiScore, playerChoice, aiChoice, roundResult, winner, restartBtn]
 
 const rock = 'rock';
 const paper = 'paper'
@@ -41,14 +44,37 @@ let computerScore = 0;
 
 buttons.forEach(btn => {
     btn.addEventListener('click', (e) => {
-        let result = playRound(e.target.id, getComputerChoice())
-        if (result.includes('AI')) {
-            computerScore++;
-        } else if (result.includes('You')) {
-            pScore++;
-        }
-        console.log('player --> ', pScore);
-        console.log('AI --> ', computerScore);
-        console.log(result);
+        let pc = e.target.id;
+        let cc = getComputerChoice()
+        if (!(pScore >= 5 || computerScore >= 5)) {
+            let result = playRound(pc, cc)
+            if (result.includes('AI')) {
+                computerScore++;
+            } else if (result.includes('You')) {
+                pScore++;
+            }
+            playerScore.textContent = 'player score -> ' + pScore;
+            aiScore.textContent = 'AI score -> ' + computerScore;
+            playerChoice.textContent = 'Player: ' + pc;
+            aiChoice.textContent = 'AI: ' + cc;
+            roundResult.textContent = result;
+            console.log('player --> ', pScore);
+            console.log('AI --> ', computerScore);
+            console.log(result);
+
+            if (pScore === 5) {
+                winner.textContent = 'You saved humanity from AI invasion'
+                restartBtn.textContent = 'RESTART'
+            } else if (computerScore === 5) {
+                winner.textContent = 'AI has taken over humanity'
+                restartBtn.textContent = 'RESTART'
+            }
+        } 
     })
+})
+
+restartBtn.addEventListener('click', () => {
+    pScore = 0;
+    computerScore = 0;
+    domElements.forEach(el => el.textContent = '')
 })
